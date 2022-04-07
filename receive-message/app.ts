60b793457788bef775adca4d125b31e72c73816f
Pablo Rosa <pablorsantos15@gmail.com>
2022-04-07 11:57:46 -0300

    feat: receive message from sqs by trigger

--- receive-message/app.ts
+++ receive-message/app.ts
@@ -1,27 +1,35 @@
-import { APIGatewayProxyResult, SQSEvent } from 'aws-lambda';
-import { SQS } from 'aws-sdk';
+import { SQSEvent } from 'aws-lambda';
 
-export const lambdaHandler = async (event: SQSEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        const sqs = new SQS();
-        const params: SQS.ReceiveMessageRequest = {
-            MaxNumberOfMessages: 10,
-            MessageAttributeNames: ['All'],
-            VisibilityTimeout: 20,
-            QueueUrl: process.env.QUEUE_URL as string,
-            WaitTimeSeconds: 5,
-        };
+export const lambdaHandler = async (event: SQSEvent): Promise<void> => {
+    event.Records.forEach((record) => {
+        console.log(record);
+    });
 
-        const data = await sqs.receiveMessage(params).promise();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data.Messages),
-        };
-    } catch (error) {
-        console.log(error);
-        return {
-            statusCode: 400,
-            body: 'Ocorreu um erro ao processar solicitação',
-        };
-    }
+    // Recebimento de mensagem via chamada http
+    // const sqs = new SQS();
+    // const params: SQS.ReceiveMessageRequest = {
+    //     MaxNumberOfMessages: 10,
+    //     MessageAttributeNames: ['All'],
+    //     VisibilityTimeout: 20,
+    //     QueueUrl: process.env.QUEUE_URL as string,
+    //     WaitTimeSeconds: 5,
+    // };
+
+    // const data = await sqs.receiveMessage(params).promise();
+
+    // if (data.Messages)
+    //     for await (const message of data.Messages) {
+    //         if (message.ReceiptHandle)
+    //             await sqs
+    //                 .deleteMessage({
+    //                     QueueUrl: process.env.QUEUE_URL as string,
+    //                     ReceiptHandle: message.ReceiptHandle,
+    //                 })
+    //                 .promise();
+    //     }
+
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify(data.Messages),
+    // };
 };
